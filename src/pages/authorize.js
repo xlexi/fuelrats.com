5ba8436e595c53ab0de68b7666d7611cdb08e314
Cameron Welter <camjwelt@gmail.com>
2020-11-21 19:20:05 +0000

    I shoulda done this the first time

--- src/pages/authorize.js
+++ src/pages/authorize.js
@@ -73,6 +73,7 @@ class Authorize extends React.Component {
 
       if (payload.redirect) {
         console.log('| redirect:', payload.redirect)
+        console.log('| response:', JSON.stringify(response))
         console.log('=============================================================================')
         pageRedirect(ctx, payload.redirect)
         return {}
